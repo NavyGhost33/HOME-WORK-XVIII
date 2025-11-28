d1dcd70432f326402f4bde65fa56bacf74c81d27
NavyGhost33 <tb12best@gmail.com>
2025-11-28 18:13:05 +0200

    Add HTML, JS.

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+
+let sideSquare = prompt("What is the length of the side of the square (sm)?");
+console.log(sideSquare);
+
+// document.write("Hello" + sideSquare + "!");
+// alert(sideSquare);
+
+let perimeter = sideSquare * 4;
+console.log(perimeter);
+document.write("Perimeter of a square = " + perimeter + " sm.");
